7d87fed6c425389ac4378fcc84ee9a0c661a0064
Abraham Galva <ag212@icloud.com>
2019-12-23 03:03:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string.split(" ").each do |word|
     return string.reverse
   end
-  binding.pry
+  
 end
 
 def reverse_each_word(string)
@@ -11,3 +11,4 @@ def reverse_each_word(string)
   end
 end
 
+binding.protect_from_forgery
